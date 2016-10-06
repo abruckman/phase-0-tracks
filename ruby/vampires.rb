6abb90fb5d6e749f42966cdd32ce332a4d95468e
abruckman <abruckman09@gmail.com>
2016-10-05 20:51:02 -0700

    add vampire interview questions take 2

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "what is your name?"
+vmpname = gets.chomp 
+puts "what year were you born?"
+year = gets.chomp.to_i 
+puts "how old are you?"
+age = gets.chomp.to_i
+puts "our company serves garlic bread. Should we order you some? (yes or no?)"
+bread = gets.chomp
+puts "would you like to enroll in the company's health insurance? (yes or no?)"
+health insurance = gets.chomp
