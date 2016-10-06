c820f97f275a92d5f66394aab5153e4f8f4dbbd9
abruckman <abruckman09@gmail.com>
2016-10-05 21:28:59 -0700

    added looping for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,72 @@
-puts "what is your name?"
-vmpname = gets.chomp 
-puts "what year were you born?"
-year = gets.chomp.to_i 
-puts "how old are you?"
-age = gets.chomp.to_i
-puts "our company serves garlic bread. Should we order you some? (yes or no?)"
-bread = gets.chomp
-puts "would you like to enroll in the company's health insurance? (yes or no?)"
-insurance = gets.chomp
+puts "how many employees are we interviewing?"
+employees = gets.chomp.to_i
+currentemp = 0
 
-agediff = (2016-year) - age 
-if agediff <= 1 && agediff >= -1   
-	ageright = true
-else
-	ageright = false
-end
+while currentemp<employees 
 
-if ageright && (bread == "yes" || insurance == "yes" )
-	vamp = 1
-end
+	puts "what is your name?"
+	vmpname = gets.chomp 
+	puts "what year were you born?"
+	year = gets.chomp.to_i 
+	puts "how old are you?"
+	age = gets.chomp.to_i
+	puts "our company serves garlic bread. Should we order you some? (yes or no?)"
+	bread = gets.chomp
+	puts "would you like to enroll in the company's health insurance? (yes or no?)"
+	insurance = gets.chomp
 
-if !ageright && (bread == "no" || insurance == "no")
-	vamp = 2
-end
 
-if !ageright && bread == "no" && insurance == "no"
-	vamp = 3
-end
+	agediff = (2016-year) - age 
+	if agediff <= 1 && agediff >= -1   
+		ageright = true
+	else
+		ageright = false
+	end
 
-if vmpname == "Drake Cula"  
-	vamp = 4
-elsif vmpname == "Tu Fang"
-	vamp = 4
-end
+	if ageright && (bread == "yes" || insurance == "yes" )
+		vamp = 1
+	end
 
-if vamp == 1
-	puts "Probably not a vampire"
-elsif vamp == 2
-	puts "Probably a vampire"
-elsif vamp == 3
-	puts "Almost certainly a vampire"
-elsif vamp == 4
-	puts "Definitely a vampire"
-else
-	puts "results inconclusive"
+	if !ageright && (bread == "no" || insurance == "no")
+		vamp = 2
+	end
+
+	if !ageright && bread == "no" && insurance == "no"
+		vamp = 3
+	end
+
+	if vmpname == "Drake Cula"  
+		vamp = 4
+	elsif vmpname == "Tu Fang"
+		vamp = 4
+	end
+
+	close = false
+	puts "do you have any allergies? list one at a time, type 'done' when finished "
+	while close = false
+		
+		allergy = gets.chomp
+		if allergy =="sunshine"
+			vamp = 4
+			close = true
+		elsif allergy == done
+			close = true
+		else
+			puts "any others?"
+		end
+		
+	end
+
+	if vamp == 1
+		puts "Probably not a vampire"
+	elsif vamp == 2
+		puts "Probably a vampire"
+	elsif vamp == 3
+		puts "Almost certainly a vampire"
+	elsif vamp == 4
+		puts "Definitely a vampire"
+	else
+		puts "results inconclusive"
+	end
+	currentemp = currentemp + 1
 end
