7917aff175e774b9c2abd979208bf20affe3abcf
abruckman <abruckman09@gmail.com>
2016-10-10 20:25:37 -0700

    added delete functions

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -45,10 +45,15 @@ bushes = {
 }
 
 lucky = [5, 14, 21, 36]
-p lucky
-lucky.delete_if { |num| num <= 15 }
-p lucky
 
-p bushes
-bushes.delete_if { |letter, number| number <=15 }
-p bushes
+# p lucky
+# lucky.delete_if { |num| num <= 15 }
+# p lucky
+
+# p bushes
+# bushes.delete_if { |letter, number| number <=15 }
+# p bushes
+
+p lucky
+lucky.keep_if { |num| num >= 15}
+p lucky
