40487e7e4d6135ded8c346e467652c594d15e173
abruckman <abruckman09@gmail.com>
2016-10-10 20:48:25 -0700

    add the drop function

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -41,10 +41,10 @@ bushes = {
 	a: 5,
 	b: 14,
 	c: 21,
-	d: 36
+	d: 35
 }
 
-lucky = [5, 14, 21, 36]
+lucky = [5, 14, 21, 35]
 
 # p lucky
 # lucky.delete_if { |num| num <= 15 }
@@ -62,10 +62,17 @@ lucky = [5, 14, 21, 36]
 # bushes.keep_if { |letter, number| number  >= 15 }
 # p bushes
 
-p lucky
-lucky.select! {|num| num >=15}
-p lucky
+# p lucky
+# lucky.select! {|num| num >=15}
+# p lucky
 
-p bushes
-bushes.select! {|letter, num| num >=15}
-p bushes
+# p bushes
+# bushes.select! {|letter, num| num >=15}
+# p bushes
+
+p lucky
+new_lucky = lucky.drop_while {|num| num % 3 != 0 }
+p lucky
+p new_lucky
+
+puts "couldn't find one that moves down the hash, becaue that doesn't make sense. Hashes aren't indexed and the order is not meaningful"
