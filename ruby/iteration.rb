83afda85a0c9990f12fedd70f58b992c39ea188a
abruckman <abruckman09@gmail.com>
2016-10-10 20:22:42 -0700

    added delete functions

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -22,17 +22,33 @@
 # p letters
 # p modified_letters
 
-meal ={
-	protien: "steak",
-	starch: "rice",
-	veggie: "green beans"
+# meal ={
+# 	protien: "steak",
+# 	starch: "rice",
+# 	veggie: "green beans"
+# }
+
+# utensils = [
+# 	"fork",
+# 	"knife",
+# 	"spoon"
+# 	] 
+# p utensils
+# utensils.map! {|thing|  thing.upcase }
+# p utensils
+
+bushes = {
+	a: 5,
+	b: 14,
+	c: 21,
+	d: 36
 }
 
-utensils = [
-	"fork",
-	"knife",
-	"spoon"
-	] 
-p utensils
-utensils.map! {|thing|  thing.upcase }
-p utensils
+lucky = [5, 14, 21, 36]
+p lucky
+lucky.delete_if { |num| num <= 15 }
+p lucky
+
+p bushes
+bushes.delete_if { |letter, number| number <=15 }
+p bushes
