540972b30ed3e06272194d89134b36e6f95a0c3b
abruckman <abruckman09@gmail.com>
2016-10-10 20:02:43 -0700

    added an array and a hash

---
+++ ruby/iteration.rb
@@ -0,0 +1,38 @@
+# def iterate
+# 	x = 3
+# 	p x
+# 	yield(x)
+# 	yield(x) 
+# 	yield(x)
+# 	p x
+# end
+
+# iterate { |x| p x = x + 1 }
+# letters = ["a", "b", "c", "d", "e"]
+# puts "Original data:"
+# p letters
+
+# ########
+# modified_letters = letters.map do |letter|
+# 	puts letter
+# 	letter.next
+# end
+
+# puts "after .map call"
+# p letters
+# p modified_letters
+
+meal ={
+	protien: "steak",
+	starch: "rice",
+	veggie: "green beans"
+}
+
+utensils = [
+	"fork",
+	"knife",
+	"spoon"
+	] 
+p utensils
+utensils.map! {|thing| p thing.upcase }
+p utensils
