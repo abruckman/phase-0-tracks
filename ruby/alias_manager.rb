db85c8ea9b3618e09846661062ff1579be6f37e5
abruckman <abruckman09@gmail.com>
2016-10-11 19:44:48 -0700

    added user input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,47 +1,59 @@
-#declare a name
+#declare a name endcoding method
 # test name "Zalet Libotum" has all the vowels and edge cases.
-ident = "Felicia Torres"
+def encode(ident)
 p ident
-#downcase the string
-ident = ident.downcase
-#turn name into a string
-ident = ident.split(' ')
-#swap the first and last name's location in the string
-ident = ident.reverse
-#return to a string
-ident = ident.join(' ')
-#turn it into an index of chars
-ident = ident.chars
-#declare an array of vowels
-vowels = ['a', 'e', 'i', 'o', 'u']
-#its best to do vowels backwards because reverse indexing works better for edge case
-vowels = vowels.reverse
-#make an array of consonants
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-#best to reverse this as well
-consonants = consonants.reverse
-#iterate through ident
-ident.map! {|letter| 
-#replacing all vowels
-	if vowels.include?(letter)
-#with the previous vowel from the array
-		letter = vowels[vowels.index(letter) - 1]
-	elsif consonants.include?(letter)
-		letter = consonants[consonants.index(letter) -1]
-		
-	else
-		letter = letter
-	end
-}
+	#downcase the string
+	ident = ident.downcase
+	#turn name into a string
+	ident = ident.split(' ')
+	#swap the first and last name's location in the string
+	ident = ident.reverse
+	#return to a string
+	ident = ident.join(' ')
+	#turn it into an index of chars
+	ident = ident.chars
+	#declare an array of vowels
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	#its best to do vowels backwards because reverse indexing works better for edge case
+	vowels = vowels.reverse
+	#make an array of consonants
+	consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+	#best to reverse this as well
+	consonants = consonants.reverse
+	#iterate through ident
+	ident.map! {|letter| 
+	#replacing all vowels
+		if vowels.include?(letter)
+	#with the previous vowel from the array
+			letter = vowels[vowels.index(letter) - 1]
+		elsif consonants.include?(letter)
+			letter = consonants[consonants.index(letter) -1]
+			
+		else
+			letter = letter
+		end
+	}
+	#rejoin the array
+	ident = ident.join
+	#last step is recapitalizing. split it at the space again
+	ident = ident.split(' ')
+	#iterate through with the .capitalize method
+	ident.map! {|word| word.capitalize! }
+	#rejoin the array
+	ident = ident.join(' ')
+	#Zalet Libotum => Mocuvan Bemiv
+	p ident
+end
 
-
-#rejoin the array
-ident = ident.join
-#last step is recapitalizing. split it at the space again
-ident = ident.split(' ')
-#iterate through with the .capitalize method
-ident.map! {|word| word.capitalize! }
-#rejoin the array
-ident = ident.join(' ')
-#Zalet Libotum => Mocuvan Bemiv
-p ident
+#driver code
+#prompt the user for their name
+puts "What is your name?"
+yourname = gets.chomp
+#set up a loop that will run until they say quit or just hit enter
+until yourname == "quit" || yourname == ""
+#run encode in the loop on their name
+	encode(yourname)
+	puts "Enter the next name to encode, or enter 'quit'"
+	yourname = gets.chomp
+end
+#prompt them for their next input
