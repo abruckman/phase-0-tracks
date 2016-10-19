874ae3aad3980523c16175bddbe83eef810693dd
abruckman <abruckman09@gmail.com>
2016-10-19 16:07:38 -0700

    made mixin methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		p words + "! ! !" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		p words + " :) " "!!!"
+# 	end
+# end
+
+# Shout.yell_happily("This is good")
+# Shout.yell_angrily("This is bad")
+
 module Shout
-	def self.yell_angrily(words)
-		p words + "! ! !" + " :("
+	def yell_angrily(words)
+		p words + "!!!" + ">:("
 	end
 
-	def self.yell_happily(words)
-		p words + " :) " "!!!"
-	end
+ 	def yell_happily(words)
+ 		p words + " :) " "!!!"
+ 	end
 end
 
-Shout.yell_happily("This is good")
-Shout.yell_angrily("This is bad")
+class Darn
+	include Shout
+end
+
+class Shucks
+	include Shout
+end
+
+darn = Darn.new
+shucks = Shucks.new
+
+darn.yell_angrily("consarn it")
+darn.yell_happily("hurrah")
+shucks.yell_angrily("Daggum")
+shucks.yell_happily("yippee")
