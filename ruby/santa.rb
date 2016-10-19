4226d7c3b7c0325f2e6f66fefcbafc396fdd1217
abruckman <abruckman09@gmail.com>
2016-10-18 18:38:13 -0700

    create a santa class with two methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,25 @@
 # create santa class
-
-#create Speak method
-
-#creat eat_milk_and_cookies method
+class Santa
 
 #initialize that prints initializing "instance Santa instnace"
+	def initialize
+		puts "initializing Santa instance..."
+	end
+
+#create Speak method
+	def speak
+		puts "Ho, ho ho! Haaaappy holidays!"
+	end
+
+#creat eat_milk_and_cookies method
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+end
 
 #test
+
+dave = Santa.new
+dave.speak
+dave.eat_milk_and_cookies("snickerdoodle")
