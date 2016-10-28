a35c4b85efde22bf5500596dc804ef20489ca396
abruckman <abruckman09@gmail.com>
2016-10-27 19:16:58 -0700

    added car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,19 +1,40 @@
-var colors = ["green", "red", "white", "orange"];
-var horses= ["jim", "larry", "moe", "tom"];
+// var colors = ["green", "red", "white", "orange"];
+// var horses= ["jim", "larry", "moe", "tom"];
 
-console.log(colors);
-console.log(horses);
+// console.log(colors);
+// console.log(horses);
 
-colors.push("brown");
-horses.push("bill");
+// colors.push("brown");
+// horses.push("bill");
 
-console.log(colors);
-console.log(horses);
+// console.log(colors);
+// console.log(horses);
 
-var horseColors = {};
+// var horseColors = {};
 
-for (var i = colors.length - 1; i >= 0; i--) {
-	horseColors[horses[i]] = colors[i]
+// for (var i = colors.length - 1; i >= 0; i--) {
+// 	horseColors[horses[i]] = colors[i]
+// }
+
+// console.log(horseColors)
+
+function Car(make, year, color) {
+	console.log("building a new car")
+
+	this.make = make;
+	this.year = year;
+	this.color = color;
+
+	this.horn = function() {
+		console.log(this.make + " " + "driver honks the horn" );
+	}
 }
 
-console.log(horseColors)
+var prius = new Car("toyota", 2010, "white");
+prius.horn();
+var mustang = new Car("ford", 2013, "red");
+var pickup = new Car("chevy", 2002, "blue" );
+
+for (var propt in prius) {
+	console.log(propt+ ":" +  prius[propt])
+}
