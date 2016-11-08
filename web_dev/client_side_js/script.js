577ee822f6148735a547134c3a766c001e59884e
abruckman <abruckman09@gmail.com>
2016-11-08 14:42:17 -0800

    added a show paragraph function

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -4,6 +4,19 @@ var pict = document.getElementById("think");
 
 function addRedBorder(event){
 	event.target.style.border = "red dashed 2px";
+	console.log(event)
 }
 
 pict.addEventListener("click", addRedBorder);
+
+var para = document.getElementById("lorem");
+
+function showParagraph(event){
+	para.style.display = "block";
+}
+var header = document.getElementById("header")
+
+header.addEventListener("click", showParagraph)
+
+para.addEventListener("click", addRedBorder)
+
