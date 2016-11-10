00bf2041175f4808c0935f52e4c3ff41bfd8f391
abruckman <abruckman09@gmail.com>
2016-11-10 12:27:16 -0800

    create search page

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,6 +1,7 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
+require "sinatra/reloader" if development?
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
@@ -22,6 +23,10 @@ end
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+get '/students/search' do
+	erb :search_students
 end
 
 # add static resources
